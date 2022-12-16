aa7a12373a8813e1c115b5db5e2ebcb27fc53607
EmmmM9O <2124363741@qq.com>
2022-12-16 14:16:15 +0000

    Ui

--- src/core/blocks/TopBlock.java
+++ src/core/blocks/TopBlock.java
@@ -1,9 +1,12 @@
 package core.blocks;
 
+import arc.graphics.Color;
 import arc.util.Log;
+import arc.util.Nullable;
 import core.Top;
 import mindustry.gen.Building;
 import mindustry.graphics.Drawf;
+import mindustry.graphics.Pal;
 import mindustry.world.Block;
 
 public class TopBlock extends Block {
@@ -16,21 +19,32 @@ public class TopBlock extends Block {
         update=true;
     }
     public class TopBuilding extends Building {
+        @Nullable
+        public Top nowTop;
         @Override
         public void drawConfigure() {
             super.drawConfigure();
             for (var i:tops){
-                Drawf.square(i.x*8+x,i.y*8+y,5f,i.color);
+                Color c=i.color;
+                if (nowTop!=null&& nowTop.x.equals(i.x) && nowTop.y.equals(i.y)) c= Pal.accent;
+                Drawf.square(i.x*8+x,i.y*8+y,5f,c);
             }
+        }
+
+        @Override
+        public void onConfigureClosed() {
+            super.onConfigureClosed();
+            nowTop=null;
         }
 
         @Override
         public boolean onConfigureTapped(float x, float y) {
 
             for (var i:tops){
-                var len=Math.sqrt((x-this.x+i.x*8f)*(x-this.x+i.x*8f)+(y-this.y+i.y*8f)*(y-this.y+i.y*8f));
+                var len=Math.sqrt((x-this.x-i.x*8f)*(x-this.x-i.x*8f)+(y-this.y-i.y*8f)*(y-this.y-i.y*8f));
                 Log.info(len);
                 if(len<=5.5d){
+                    nowTop=i;
                     Log.info(i.name);
                     return true;
                 }
---
+++ src/core/ui/GuoLvQi.java
@@ -0,0 +1,15 @@
+package core.ui;
+
+import arc.scene.ui.layout.Table;
+import mindustry.type.Item;
+
+import java.util.Vector;
+
+public class GuoLvQi {
+    public Table table;
+    public Vector<Item> items;
+    public void init(){
+        table.reset();
+
+    }
+}
