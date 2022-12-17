456ba1a3df0999aeeecef374b856841cff37dc6e
EmmmM9O <111637956+EmmmM9O@users.noreply.github.com>
2022-12-18 06:26:08 +0800

    Update ItemSeletors.java

--- src/core/ui/tables/ItemSeletors.java
+++ src/core/ui/tables/ItemSeletors.java
@@ -1,7 +1,10 @@
 package core.ui.tables;
 
 import core.ui.WorldTable;
+
 import mindustry.type.Item;
+
+import arc.util.Log;
 
 import java.util.Vector;
 
@@ -12,16 +15,31 @@ public class ItemSeletors extends WorldTable {
     public void chooseRun(Item item){
 
     }
+
+    @Override
+
     public void draw(){
+
+        Log.info("draw");
+
         table.pane((t ->{
+
             int row=0;
+
             for (var i:list){
+
                 row++;
 
                 t.image(i.fullIcon);
+
                 t.button("X",()->chooseRun(i));
+
                 if(row>=7) t.row();
+
             }
+
         }));
+
     }
+
 }
