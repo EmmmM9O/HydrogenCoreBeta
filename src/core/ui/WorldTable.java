2d0d867ceb40edf33872f5cdbc629e57a1a7ac33
EmmmM9O <2124363741@qq.com>
2022-12-19 02:23:50 +0000

    up ui

--- src/core/ui/WorldTable.java
+++ src/core/ui/WorldTable.java
@@ -4,6 +4,7 @@ import arc.scene.ui.layout.Table;
 
 
 public class WorldTable {
+    public String name;
     public void draw(Table table){
 
     }
--- src/core/ui/tables/Leach.java
+++ src/core/ui/tables/Leach.java
@@ -2,16 +2,28 @@ package core.ui.tables;
 
 import arc.graphics.Color;
 import arc.scene.ui.layout.Table;
+import arc.util.Nullable;
 import core.ui.WorldTable;
-import mindustry.gen.Icon;
+
+import java.util.Vector;
 
 
 public class Leach extends WorldTable {
+    public Vector<WorldTable> paper=new Vector<>();
+    @Nullable
+    public WorldTable now;
 
     @Override
     public void draw(Table table) {
         table.setColor(Color.gray);
         table.add("Leach");
-
+        for (var i:paper){
+            table.button(i.name,()->now=i);
+        }
+        if(now==null)return;
+        table.pane(t->{
+            t.add(now.name);
+            now.draw(t);
+        });
     }
 }
